edc12eb8eeda9f465c64a03bd59b73f61fb28497
Sylvia Shen <azncreedii@gmail.com>
2017-05-16 15:00:57 +1000

    initial commit

---
+++ src/Results.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+	render() {
+		return(
+		  <div>
+			<h3>{this.props.end_message}</h3>
+			<p> Your score was: {this.props.score}</p>
+			<button onClick={this.props.handleRetry}>Retry</button>
+		  </div>
+		) 
+
+		}
+	}
+
+
+export default Results;
